8dd354c4bfd805a31c7e15d9b22073a61176124b
AlexisRenchon <a.renchon@gmail.com>
2020-01-17 16:59:39 -0600

    Create Some_text.java

---
+++ Some_text.java
@@ -0,0 +1,11 @@
+/* This is a simple Java program. 
+   FileName : "Some_text.java". */
+   class Some_text 
+   { 
+       // Your program begins with a call to main(). 
+       // Prints "Some text" to the terminal window. 
+       public static void main(String args[]) 
+       { 
+           System.out.println("Some text"); 
+       } 
+   } 
